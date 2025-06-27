690cf199f0dfdf0eeb69d639869ead59f0779f80
YeikoDev <144139495+YeikoD@users.noreply.github.com>
2025-06-27 14:00:18 -0300

    Eliminar(scripts) se eliminaron script obsoletos

--- Assets/Scripts/NPC/GrafoIA.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-using XNode;
-
-[CreateAssetMenu(fileName = "NuevoGrafoIA", menuName = "IA/Grafo Principal")]
-public class GrafoIA : NodeGraph { }
--- Assets/Scripts/NPC/NodoIA.cs
+++
@@ -1,7 +0,0 @@
-using XNode;
-
-public abstract class NodoIA : Node
-{
-	// Este método va a ser ejecutado por el NPC para decidir qué hacer
-	public abstract NodoIA Evaluar(NPC npc);
-}
